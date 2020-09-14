e32359af50fea2f96d4fba7885040cddb52e0d24
WADEOS <wade4615@gmail.com>
2020-09-14 05:51:50 -0500

    refactoring and bug fix

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -10,35 +10,37 @@
 #include <limits>
 #include "Matrix.h"
 
-class NeuralNetwork {
-        Array<double> *inputLayer;
-        Matrix<double> *inputMiddleWeights;
-        Array<double> *middleLayer;
-        Matrix<double> *middleOutputWeights;
-        Array<double> *outputLayer;
-        IndexType inputSize;
-        IndexType middleSize;
-        IndexType outputSize;
-        IndexType numTrainingSets;
-        Array<double> *hiddenLayerBias;
-        Array<double> *outputLayerBias;
-        Matrix<double> *trainingInput;
-        Matrix<double> *trainingOutput;
-        Array<double> *deltaHidden;
-        Array<double> *deltaOutput;
-    public:
-        NeuralNetwork(IndexType inputSize, IndexType middleSize, IndexType outputSize);
-        ~NeuralNetwork();
-        void loadTrainingData(Matrix<double> *input, Matrix<double> *output);
-        void train(IndexType epochs);
-        IndexType getInputSize();
-        IndexType getMiddleSize();
-        IndexType getOutputSize();
-    private:
-        void shuffle(int *array, size_t n);
-        double sigmoid(double x);
-        double sigmoidDerivative(double x);
-
+class NeuralNetwork
+{
+    Array<double> *inputLayer;
+    Matrix<double> *inputMiddleWeights;
+    Array<double> *middleLayer;
+    Matrix<double> *middleOutputWeights;
+    Array<double> *outputLayer;
+    IndexType inputSize;
+    IndexType middleSize;
+    IndexType outputSize;
+    IndexType numTrainingSets;
+    Array<double> *hiddenLayerBias;
+    Array<double> *outputLayerBias;
+    Matrix<double> *trainingInput;
+    Matrix<double> *trainingOutput;
+    Array<double> *deltaHidden;
+    Array<double> *deltaOutput;
+public:
+    NeuralNetwork(IndexType inputSize, IndexType middleSize, IndexType outputSize);
+    ~NeuralNetwork();
+    void loadTrainingData(Matrix<double> *input, Matrix<double> *output);
+    void train(IndexType epochs);
+    IndexType getInputSize();
+    IndexType getMiddleSize();
+    IndexType getOutputSize();
+private:
+    void shuffle(int *array, size_t n);
+    double sigmoid(double x);
+    double sigmoidDerivative(double x);
+    Array<double>& sigmoid(Array<double> *layerIn);
+    Array<double>& sigmoidDerivative(Array<double> *layerIn);
 };
 
 #endif // NEURALNETWORK_H_INCLUDED
